0834105f1bdc1dfbbd76cb9ed172d35c8b123ba5
EktorGG <profesorhectorg@gmail.com>
2022-09-15 21:56:58 -0400

    resolviendo css

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
     //Similar a querySelectorAll()
-    $("#cabecera").load("/html/parts/header.html");
-    $("#pie-pagina").load("/html/parts/footer.html");
+    $("#cabecera").load("./html/parts/header.html");
+    $("#pie-pagina").load("./html/parts/footer.html");
 });
